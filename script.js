e1da857fa617b352e2c3ea9d264b0a322503ff1d
4Lorne <lornedayy@gmail.com>
2023-02-23 23:47:18 -0400

    Adding firebase

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-    $('.accordion-collapse').on('hidden.bs.collapse', function () {
+    $('.accordion-button').on('hidden.bs.collapse', function () {
         // find the children and close them
         $(this).find('.collapse').collapse('hide');
     });
 });
 
+//Bug that closes all of them sometimes, probably has to do with selector
