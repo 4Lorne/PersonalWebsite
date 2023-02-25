53c44ba7144f5cc90b02802feb2fe845f28b3b2a
4Lorne <lornedayy@gmail.com>
2023-02-24 21:33:07 -0400

    Added feature to close child accordions when parent closes

--- public/script.js
+++ public/script.js
@@ -1,7 +1,5 @@
-$(document).ready(function(){
-    $('.accordion-button').on('hidden.bs.collapse', function () {
-        // find the children and close them
-        $(this).find('.collapse').collapse('hide');
-    });
+$('.accordion-collapse').on('hidden.bs.collapse', function () {
+    // find the children and close them
+    $(this).find('.collapse').collapse('hide');
 });
 
