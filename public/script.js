af9408bf32e0c0863fe33333fd8a3ca25845c8b5
4Lorne <lornedayy@gmail.com>
2023-02-27 17:03:18 -0400

    Fixed colored arrow issue

--- public/script.js
+++ public/script.js
@@ -1,4 +1,15 @@
-$('.accordion-collapse').on('hidden.bs.collapse', function () {
+//Closes the children accordions
+$('.accordion-collapse').on('hidden.bs.collapse', function() {
     // find the children and close them
     $(this).find('.collapse').collapse('hide');
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
